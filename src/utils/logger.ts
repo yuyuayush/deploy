9be08df35cf7bff17a4f109d554f69eeb044b9de
ayush negi <ayushnegi1912@gmail.com>
2026-09-10 18:15:49 +0530

    add

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,31 +1,17 @@
 import pino from 'pino';
-import { createRequire } from 'module';
 import { env } from '../config/env.js';
-
-const require = createRequire(import.meta.url);
-
-const getTransport = () => {
-  if (env.NODE_ENV !== 'development') {
-    return undefined;
-  }
-
-  try {
-    require.resolve('pino-pretty');
-    return {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-      },
-    };
-  } catch {
-    return undefined;
-  }
-};
 
 export const logger = pino({
   level: env.LOG_LEVEL,
-  transport: getTransport(),
+  transport:
+    env.NODE_ENV === 'development'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        }
+      : undefined,
 });
-
