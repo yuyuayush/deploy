ab6391ef556457b7d0e292d0f840fd1afdf18944
ayush negi <ayushnegi1912@gmail.com>
2026-09-12 20:29:50 +0530

    cors-fizx

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -32,5 +32,9 @@ export const auth = betterAuth({
   },
   secret: env.BETTER_AUTH_SECRET,
   baseURL: env.BETTER_AUTH_URL,
-  trustedOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000',env.CORS_ORIGIN],
+  trustedOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    ...(env.CORS_ORIGIN ? [env.CORS_ORIGIN] : []),
+  ],
 });
