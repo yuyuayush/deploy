f8c2851ab45266cbb04dff9e137ab5a814091965
ayush negi <ayushnegi1912@gmail.com>
2026-09-12 20:24:19 +0530

    added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -32,5 +32,5 @@ export const auth = betterAuth({
   },
   secret: env.BETTER_AUTH_SECRET,
   baseURL: env.BETTER_AUTH_URL,
-  trustedOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  trustedOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000',env.CORS_ORIGIN],
 });
